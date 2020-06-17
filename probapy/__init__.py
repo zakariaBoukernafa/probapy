4da361573b6ad1bf9cafd26799f90c47f158c03f
zakariaBoukernafa <zakaria0697@gmail.com>
2020-06-17 16:27:53 +0100

    first commit

---
+++ probapy/__init__.py
@@ -0,0 +1,3 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probapy',
+      version='0.1',
+      description='Gaussian and binomial distributions package',
+      packages=['probapy'],
+      author = 'zakaria boukernafa',
+      author_email = 'zakaria0697@gmail.com',
+      zip_safe=False)
